12fb96518ea6273081196efb71bc0eee4ca149af
cand04 <e41231578@student.polije.ac.id>
2025-02-23 18:53:05 +0700

    acara 8(eror) & 9

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ManagementUserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,59 +23,59 @@ use Illuminate\Support\Facades\Route;
 
 // Route lainnya
 
-// Route::get('/foo', function () {
-//     return 'Hello, world!';
-// });
+Route::get('/foo', function () {
+    return 'Hello, world!';
+});
 
-// Route::get('/foo/{id}', function ($id) {
-//     return 'User = ' . $id;
-// });
+Route::get('/foo/{id}', function ($id) {
+    return 'User = ' . $id;
+});
 
-// Route::get('/profile', function () {
-//     return view('profile', [
-//         'nama'  => 'Candra Puji Utama',
-//         'nim'   => 'E41231578',
-//         'prodi' => 'Teknik Informatika'
-//     ]);
-// });
+Route::get('/profile', function () {
+    return view('profile', [
+        'nama'  => 'Candra Puji Utama',
+        'nim'   => 'E41231578',
+        'prodi' => 'Teknik Informatika'
+    ]);
+});
 
-// Route::get('/user/{name?}', function ($name = 'Guest') {
-//     return "Hello, $name!";
-// });
+Route::get('/user/{name?}', function ($name = 'Guest') {
+    return "Hello, $name!";
+});
 
-// Route::get('/user/name/{name}', function ($name) {
-//     return "Hello, $name!";
-// })->where('name', '[A-Za-z]+');
+Route::get('/user/name/{name}', function ($name) {
+    return "Hello, $name!";
+})->where('name', '[A-Za-z]+');
 
-// Route::get('/user/id/{id}', function ($id) {
-//     return "User ID: $id";
-// })->where('id', '[0-9]+');
+Route::get('/user/id/{id}', function ($id) {
+    return "User ID: $id";
+})->where('id', '[0-9]+');
 
-// Route::get('/user/details/{id}/{name}', function ($id, $name) {
-//     return "User ID: $id, Name: $name";
-// })->where(['id' => '[0-9]+', 'name' => '[A-Za-z]+']);
+Route::get('/user/details/{id}/{name}', function ($id, $name) {
+    return "User ID: $id, Name: $name";
+})->where(['id' => '[0-9]+', 'name' => '[A-Za-z]+']);
 
-// Route::get('/search/{query}', function ($query) {
-//     return "Search result for: $query";
-// })->where('query', '.*');
+Route::get('/search/{query}', function ($query) {
+    return "Search result for: $query";
+})->where('query', '.*');
 
-// // Redirect Route
-// Route::redirect('/coba', '/sini');
+// Redirect Route
+Route::redirect('/coba', '/sini');
 
-// // Route untuk profile
-// Route::get('/user/profile', [UserController::class, 'show'])->name('profile.user');
+// Route untuk profile
+Route::get('/user/profile', [UserController::class, 'show'])->name('profile.user');
 
-// // acara 5
-// // Mendefinisikan rute untuk 'user' yang mengarah ke metode 'index' dari controller 'ManagementUserController'
-// Route::get('user', [ManagementUserController::class, 'index']); 
+// acara 5
+// Mendefinisikan rute untuk 'user' yang mengarah ke metode 'index' dari controller 'ManagementUserController'
+Route::get('user', [ManagementUserController::class, 'index']); 
 
-// // Jika Anda ingin menggunakan resource controller untuk user, gunakan Route::resource
-// Route::resource('user', ManagementUserController::class);
+// Jika Anda ingin menggunakan resource controller untuk user, gunakan Route::resource
+Route::resource('user', ManagementUserController::class);
 
-// // Rute untuk halaman depan aplikasi
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// Rute untuk halaman depan aplikasi
+Route::get('/', function () {
+    return view('welcome');
+});
 
 //acara 6
 Route::get('/home', function () {
@@ -88,3 +88,9 @@ route::group(['namespace' => 'App\Http\Controllers\frontend'], function()
 {
     route::resource('home', 'HomeController');
 });
+
+//acara 8 (eror)
+Route::namespace('Backend')->group(function () {
+    Route::resource('dashboard', 'DashboardController');
+});
+
