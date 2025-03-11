3235ec406cd17d71b9c26c0d8b8d9108709facf3
cand04 <e41231578@student.polije.ac.id>
2025-03-11 23:26:38 +0700

    acara 21(tadi salah ketik haruanya 21 malah 20)

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,11 @@ use App\Http\Controllers\Backend\ApiPendidikanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Middleware autentikasi untuk endpoint /user
+
 Route::middleware('auth:api')->get('/user', function(Request $request) {
     return $request->user();
 });
 
-// Rute API dengan prefix 'v1'
 Route::prefix('v1')->group(function() {
     Route::get('/api_pendidikan', [ApiPendidikanController::class, 'getAll']);  // Mendapatkan semua data pendidikan
     Route::get('/api_pendidikan/{id}', [ApiPendidikanController::class, 'getPen']);  // Mendapatkan data pendidikan berdasarkan ID
